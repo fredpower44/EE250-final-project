f746d6991c3f1d4f1291ac3dbfdf4659dc849e20
Frederick Zhang <fyzhang@usc.edu>
2020-11-03 23:16:11 -0800

    Finished GameClient

--- ee250/hangman/GameClient.py
+++ ee250/hangman/GameClient.py
@@ -1,7 +1,24 @@
 import requests
 import paho.mqtt.client as mqtt
+import time
 
 
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+    client.subscribe("fyzhang/guess")
+    client.message_callback_add("fyzhang/guess", guessCallback)
+
+def guessCallback(client, userdata, msg):
+	guess = str(msg.payload, "utf-8")
+	global guessQueue
+	guessQueue.append(guess)
+	print(guessQueue)
+
+def on_message(client, userdata, msg):
+    print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
+
+#Uses wordnik randomWords API to generate a random word
 def getRandomWord():
 	word = '-'
 	while not word.isalnum():
@@ -17,15 +34,61 @@ def getRandomWord():
 			word = data[0]['word']
 	return word
 
-def main():
-	client = mqtt.Client()
+#reveals letters in hiddenWord if guess is correct
+def guessLetter(guess):
+	global word
+	global hiddenWord
+	print(word)
+	while guess in word:
+		ind = int(word.find(guess))
+		hiddenWord = hiddenWord[0:ind] + word[ind] + hiddenWord[ind+1:len(word)]
+		word = word[0:ind] + '*' + word[ind+1:len(word)]
 
+#main method
+if __name__ == "__main__":
+	#initialize all global variables
+	global guessQueue
+	global word
+	global hiddenWord
+	global fullWord
 	word = getRandomWord()
-	# print(word)
+	word = 'ruffled'
+	fullWord = word
+	print(word)
 	hiddenWord = ''
 	while len(hiddenWord) < len(word):
 		hiddenWord = hiddenWord + '_'
+	guessQueue = []
 
+	#initialize mqtt client
+	client = mqtt.Client()
+	client.on_message = on_message
+	client.on_connect = on_connect
+	client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+	client.loop_start()
+	time.sleep(2)
 
-if __name__ == "__main__":
-	main()
+	#pull from the guessQueue and apply the guesses
+	lives = 6
+	while lives > 0:
+		print("You have " + str(lives) + " guesses left. Guess any letter:")
+		print(hiddenWord)
+		while len(guessQueue) == 0:
+			time.sleep(0.1)
+		# guess = input()
+		guess = guessQueue.pop(0)
+		if guess in word:
+			guessLetter(guess)
+			client.publish("fyzhang/correct", "CORRECT")
+		else:
+			lives -= 1
+			client.publish("fyzhang/correct", "INCORRECT")
+		if hiddenWord == fullWord:
+			break
+
+	#Print win/lose game message
+	if lives == 0:
+		print("You have no more lives. You lost.")
+	else:
+		print("Congratulations! You won!")
+	print("The word was: " + fullWord)
---
+++ ee250/hangman/RemoteClient.py
@@ -0,0 +1,61 @@
+"""EE 250L Lab 04 Starter Code
+
+Team members: Frederick Zhang
+Github: http://github.com/usc-ee250-fall2020/lab05-lab5_fred
+
+Run rpi_pub_and_sub.py on your Raspberry Pi."""
+
+import paho.mqtt.client as mqtt
+import time
+import sys
+sys.path.append('../../Software/Python/')
+sys.path.append('../../Software/Python/grove_rgb_lcd')
+# import grovepi
+# import grove_rgb_lcd
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+    client.subscribe("RPI_fyzhang/correct")
+    client.message_callback_add("fyzhang/correct", correctCallback)
+
+#Default message callback. Please use custom callbacks.
+def on_message(client, userdata, msg):
+    print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
+
+#callback to check correctness
+def correctCallback(client, userdata, msg):
+    if str(msg.payload, "utf-8") == "CORRECT":
+        print("Right")
+    elif str(msg.payload, "utf-8") == "INCORRECT":
+        print("Wrong")
+
+if __name__ == '__main__':
+    #this section is covered in publisher_and_subscriber_example.py
+    client = mqtt.Client()
+    client.on_message = on_message
+    client.on_connect = on_connect
+    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+    client.loop_start()
+
+    while True:
+        guess = input()
+        client.publish("fyzhang/guess", guess)
+        print("done")
+    # #defining the ports
+    # Ranger = 4
+    # LED = 3
+    # Button = 2
+
+    # #setting background to light blue for lcd
+    # grove_rgb_lcd.setRGB(0, 64, 32)
+    # grove_rgb_lcd.textCommand(1)
+
+    # while True:
+    #     client.publish("RPI_fyzhang/ultrasonicRanger", str(grovepi.ultrasonicRead(Ranger)))
+
+    #     if grovepi.digitalRead(Button) == 1:
+    #         client.publish("RPI_fyzhang/button", "Button pressed!")
+
+    #     time.sleep(1)
+            
+
