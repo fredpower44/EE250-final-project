4b27071ecf113dc07c33766293179ae195aa9f91
Frederick Zhang <fyzhang@usc.edu>
2020-11-04 16:08:01 -0800

    Remote starter

--- ee250/hangman/RemoteClient.py
+++ ee250/hangman/RemoteClient.py
@@ -1,10 +1,3 @@
-"""EE 250L Lab 04 Starter Code
-
-Team members: Frederick Zhang
-Github: http://github.com/usc-ee250-fall2020/lab05-lab5_fred
-
-Run rpi_pub_and_sub.py on your Raspberry Pi."""
-
 import paho.mqtt.client as mqtt
 import time
 import sys
@@ -55,46 +48,31 @@ def correctBuzzer():
     grovepi.digitalWrite(PORT_BUZZER, 0)
 
 if __name__ == '__main__':
-    #this section is covered in publisher_and_subscriber_example.py
-    client = mqtt.Client()
-    client.on_message = on_message
-    client.on_connect = on_connect
-    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
-    client.loop_start()
+    try:
+        client = mqtt.Client()
+        client.on_message = on_message
+        client.on_connect = on_connect
+        client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+        client.loop_start()
 
-    PORT_BUTTON = 4 #D4
-    PORT_ROTARY = 0 #A0
-    PORT_BUZZER = 3 #D3
+        PORT_BUTTON = 4 #D4
+        PORT_ROTARY = 0 #A0
+        PORT_BUZZER = 3 #D3
 
-    lcd.setRGB(0,32,0)
+        lcd.setRGB(0,32,0)
 
-    letter = chr(0)
-    while True:
-        letterValue = int(grovepi.analogRead(PORT_ROTARY) / 39.385)
-        nextLetter = chr(97 + letterValue)
-        if nextLetter != letter:
-            letter = nextLetter
-            lcd.setText_norefresh("Your guess: " + letter)
-        if grovepi.digitalRead(PORT_BUTTON):
-            client.publish("fyzhang/guess", str(letter))
-            while grovepi.digitalRead(PORT_BUTTON):
-                time.sleep(0.1)
-
-    # #defining the ports
-    # Ranger = 4
-    # LED = 3
-    # Button = 2
-
-    # #setting background to light blue for lcd
-    # grove_rgb_lcd.setRGB(0, 64, 32)
-    # grove_rgb_lcd.textCommand(1)
-
-    # while True:
-    #     client.publish("RPI_fyzhang/ultrasonicRanger", str(grovepi.ultrasonicRead(Ranger)))
-
-    #     if grovepi.digitalRead(Button) == 1:
-    #         client.publish("RPI_fyzhang/button", "Button pressed!")
-
-    #     time.sleep(1)
-            
-
+        letter = chr(0)
+        while True:
+            letterValue = int(grovepi.analogRead(PORT_ROTARY) / 39.385)
+            nextLetter = chr(97 + letterValue)
+            if nextLetter != letter:
+                letter = nextLetter
+                lcd.setText_norefresh("Your guess: " + letter)
+            if grovepi.digitalRead(PORT_BUTTON):
+                client.publish("fyzhang/guess", str(letter))
+                while grovepi.digitalRead(PORT_BUTTON):
+                    time.sleep(0.1)
+    except KeyboardInterrupt:
+        lcd.setText('')
+        lcd.setRGB(0,0,0)
+        grovepi.digitalWrite(PORT_BUZZER, 0)
